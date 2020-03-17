42f8a0ebb3f7f84bd4850a90cfc68677471b82d8
papy13 <32386873+papy13@users.noreply.github.com>
2020-03-17 21:46:14 +0100

    Update function.js

--- function.js
+++ function.js
@@ -1,3 +1,7 @@
+let KmonoTri =[
+    
+]
+
 let table=[
     {section:0.5,
      natureTroncon:"aerien isole",
@@ -144,133 +148,452 @@ let table=[
         intensiteNominaleMono:1
        },
 
-    
-    
-    
+       {section:25,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:97,
+        intensiteNominaleMono:108
+       },
+       {section:25,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:110,
+        intensiteNominaleMono:126
+       },
+       {section:25,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:127,
+        intensiteNominaleMono:142
+       },
+       {section:25,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:141,
+        intensiteNominaleMono:162
+       },
+
+
+       {section:35,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:118,
+        intensiteNominaleMono:138
+       },
+       {section:35,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:133,
+        intensiteNominaleMono:152
+       },
+       {section:35,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:157,
+        intensiteNominaleMono:175
+       },
+       {section:35,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:170,
+        intensiteNominaleMono:195
+       },
+
+       {section:50,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:141,
+        intensiteNominaleMono:0
+       },
+       {section:50,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:159,
+        intensiteNominaleMono:0
+       },
+       {section:50,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:190,
+        intensiteNominaleMono:0
+       },
+       {section:50,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:204,
+        intensiteNominaleMono:0
+       },
+       
+       {section:70,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:180,
+        intensiteNominaleMono:0
+       },
+       {section:70,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:197,
+        intensiteNominaleMono:0
+       },
+       {section:70,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:242,
+        intensiteNominaleMono:0
+       },
+       {section:70,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:252,
+        intensiteNominaleMono:0
+       },
+
+       {section:95,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:258,
+        intensiteNominaleMono:0
+       },
+       {section:95,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:236,
+        intensiteNominaleMono:0
+       },
+       {section:95,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:293,
+        intensiteNominaleMono:0
+       },
+       {section:95,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:302,
+        intensiteNominaleMono:0
+       },
+
+       {section:150,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:309,
+        intensiteNominaleMono:0
+       },
+       {section:150,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:301,
+        intensiteNominaleMono:0
+       },
+       {section:150,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:390,
+        intensiteNominaleMono:0
+       },
+       {section:150,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:386,
+        intensiteNominaleMono:0
+       },
+
+       {section:240,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:415,
+        intensiteNominaleMono:0
+       },
+       {section:240,
+        natureTroncon:"Souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaleTri:393,
+        intensiteNominaleMono:0
+       },
+       {section:240,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:522,
+        intensiteNominaleMono:0
+       },
+       {section:240,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:504,
+        intensiteNominaleMono:0
+       },
+       {section:300,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:595,
+        intensiteNominaleMono:0
+       },
+       {section:300,
+        natureTroncon:"Souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaleTri:571,
+        intensiteNominaleMono:0
+       }]
+       
+
+
+let KfMonoPhase=[
+    {
+        minCumul:1,
+        maxCumul:3,
+        kf:1
+    },
+    {
+        minCumul:4,
+        maxCumul:6,
+        kf:0.78
+    },
+    {
+        minCumul:7,
+        maxCumul:9,
+        kf:0.63
+    },
+    {
+        minCumul:10,
+        maxCumul:13,
+        kf:0.53
+    },
+    {
+        minCumul:14,
+        maxCumul:16,
+        kf:0.49
+    },
+    {
+        minCumul:17,
+        maxCumul:19,
+        kf:0.46
+    },
+    {
+        minCumul:20,
+        maxCumul:23,
+        kf:0.44
+    },
+    {
+        minCumul:24,
+        maxCumul:26,
+        kf:0.42
+    },
+    {
+        minCumul:27,
+        maxCumul:33,
+        kf:0.41
+    },
+    {
+        minCumul:34,
+        kf:0.4
+    }
+
+
 ]
 
-console.log(table)
-let Tab = [];
-let Max = tab.length;
-let CosPhi = "Valeur à recup du form 1";
-let NatReseau = "Valeur Type réseau Mono ou Tri à recup du form 1"
-let U0 = "Valeur Tension Initiale à recup du form1";
-let P0 = "Puissance par client à recup du form1";
-let X1 = 0;
-let R0 = 0;
 
-function PuissanceNoeud (i)
-{
-     let res = P0*Tab[i].NbAbonneNoeud
-     return res ;
-}
-
-function Cumul(i,ColName)
-{
-    /* Cumul à partir de l'indice i (noeud) des données d'une Colonne ColName
-    (Colonne du Tableau de Cartographie) donnée en string*/
-    let Cumul = 0;
-    for (let k = i; k< Max; k++)
+let kfTriphase = [
     {
-        Cumul+= Tab[k].ColName;
-    }
-    return Cumul;
-}
+        minCumul:1,
+        maxCumul:4,
+        kf:1
+    },
+    {
+        minCumul:5,
+        maxCumul:9,
+        kf:0.78
+    },
+    {
+        minCumul:10,
+        maxCumul:14,
+        kf:0.63
+    },
+    {
+        minCumul:15,
+        maxCumul:19,
+        kf:0.53
+    },
+    {
+        minCumul:20,
+        maxCumul:24,
+        kf:0.49
+    },
+    {
+        minCumul:25,
+        maxCumul:29,
+        kf:0.46
+    },
+    {
+        minCumul:30,
+        maxCumul:34,
+        kf:0.44
+    },
+    {
+        minCumul:35,
+        maxCumul:39,
+        kf:0.42
+    },
+    {
+        minCumul:40,
+        maxCumul:49,
+        kf:0.41
+    },
+    {
+        minCumul:50,
+        kf:0.4
+    },
+]
 
-function CumulPuissanceFoisonnee(kf,i,colName){
-    let cumul = Cumul(i,colName);
+
+let lengthTable = table.length;
+
+
+ let Tab = [];
+ let Max = tab.length;
+ let CosPhi = "Valeur à recup du form 1";
+ let NatReseau = "Valeur Type réseau Mono ou Tri à recup du form 1"
+ let U0 = "Valeur Tension Initiale à recup du form1";
+ let P0 = "Puissance par client à recup du form1";
+ let X1 = 0;
+ let R0 = 0;
+ let natureTroncon = 0;
+ let section = 0 ;
+ let ameTroncon =  0  ; 
+ function PuissanceNoeud (i)
+ {
+      let res = P0*Tab[i].NbAbonneNoeud
+      return res ;
+ }
+
+ function Cumul(i,ColName)
+ {
+     /* Cumul à partir de l'indice i (noeud) des données d'une Colonne ColName
+     (Colonne du Tableau de Cartographie) donnée en string*/
+     let Cumul = 0;
+     for (let k = i; k< Max; k++)
+     {
+         Cumul+= Tab[k].ColName;
+     }
+     return Cumul;
+ }
+
+ function CumulPuissanceFoisonnee(kf,i,colName){
+     let cumul = Cumul(i,colName);
      let res = kf*cumul*P0;
-     return res;
- }
+      return res;
+  }
+
+  function CumulPuissanceNoeud(kf ,i, colName1 , colName2){
+      let CumPuiFois = CumulPuissanceFoisonnee(kf,i,colName1,Po); // Colname1 parce que la puissance foisonnée concerne le nombre d'abbonnes au noeuds 
+      let cumulChargePro = Cumul(i,colName2); // ColName 2 : Cumul charge de puissance 
+      let res = CumPuiFois + cumulChargePro;
+      return res;
+  }
 
 
- function CumulPuissanceNoeud(kf ,i, colName1 , colName2){
-     let CumPuiFois = CumulPuissanceFoisonnee(kf,i,colName1,Po); // Colname1 parce que la puissance foisonnée concerne le nombre d'abbonnes au noeuds 
-     let cumulChargePro = Cumul(i,colName2); // ColName 2 : Cumul charge de puissance 
-     let res = CumPuiFois + cumulChargePro;
-     return res;
- }
 
-
-
-//Chute de tension au nœud actuel
-function ChuteTensionNoeud(i, CumulPuiss){
-    /*E nfonction de l'indice du noeud dans le tableau, on recupère les paramètres tels que la réactance X1 et la résistivité R0 
-      puis on calcule les Chutes de Tension au noeud i*/
-    //CumulPuiss = Cumul de la puissance au noeud i à calculer à l'aide dela fonction CumulPuissanceNoeud
-    if (Tab[i].Nature === "AerienNu"){
-        X1 = 0,35 ;
-    }
-    else if (Tab[i].Nature === "AerienIsole" || "Souterrain"){
-        X1 = 0,1 ;
-    }
-
+ // Chute de tension au nœud actuel
+ function ChuteTensionNoeud(i, CumulPuiss){
+     /*E nfonction de l'indice du noeud dans le tableau, on recupère les paramètres tels que la réactance X1 et la résistivité R0 
+       puis on calcule les Chutes de Tension au noeud i*/
+     //CumulPuiss = Cumul de la puissance au noeud i à calculer à l'aide dela fonction CumulPuissanceNoeud
+     if (Tab[i].Nature === "AerienNu"){
+         X1 = 0,35 ;
+     }
+     else if (Tab[i].Nature === "AerienIsole" || "Souterrain"){
+         X1 = 0,1 ;
+     }
     if (Tab[i].Ame === "Aluminium"){
-        R0 = 30 ;
-    }
-    else if (Tab[i].Nature === "Cuivre"){
-        R0 = 18 ;
-    }
-        
-    let Result = CumulPuiss*Tab[i].Nature*((R0/Tab[i].Section) + X1*Math.sqrt((1/(CosPhi*CosPhi))-1))/TensionNoeudActuel(i-1);
-    
-    if(NatReseau === "Triphasé"){
-        //Chute de tension au nœud actuel - Triphasé
-        return Result;
-    }
-    else if (NatReseau === "Monophase"){
-        //Chute de tension au nœud actuel - Monophasé
-        return 2*Result;
-    }
-}
-
+         R0 = 30 ;
+     }
+     else if (Tab[i].Nature === "Cuivre"){
+         R0 = 18 ;
+     }
+       
+     let Result = CumulPuiss*Tab[i].Nature*((R0/Tab[i].Section) + X1*Math.sqrt((1/(CosPhi*CosPhi))-1))/TensionNoeudActuel(i-1);
+  
+     if(NatReseau === "Triphasé"){
+         //Chute de tension au nœud actuel - Triphasé
+         return Result;
+     }
+     else if (NatReseau === "Monophase"){
+         //Chute de tension au nœud actuel - Monophasé
+         return 2*Result;
+     }
+ }
 //Cumul chute de tension au noeud actuel
 
-function CumulChuteTensionNoeudActuel(i){
- let CumulTensionAmont = 0;
- let chuteNoeud = ChuteTensionNoeud(i);
- // Calcul cumulTension noeud Tension
- 
- for(let k=i ; k>0 ; k--){
-  CumulTensionAmont +=ChuteTensionNoeud(k) 
+ function CumulChuteTensionNoeudActuel(i){
+  let CumulTensionAmont = 0;
+  let chuteNoeud = ChuteTensionNoeud(i);
+  // Calcul cumulTension noeud Tension
+  for(let k=i ; k>0 ; k--){
+   CumulTensionAmont +=ChuteTensionNoeud(k) 
+  }
+  let res = chuteNoeud + CumulTensionAmont;
+  return res ;
  }
- let res = chuteNoeud + CumulTensionAmont;
- return res ;
-}
-// TensionNoeudActuel
-function TensionNoeudActuel(i){
+ TensionNoeudActuel
+ function TensionNoeudActuel(i){
  
- if(i==0 ){
-  return res = U0;
-}
- else{
-      let tensionAmont = TensionNoeudAmont(i-1);
-      let chuteTensionNoeudActuel = ChuteTensionNoeud(i);
-      let result = tensionAmont - chuteTensionNoeudActuel;
- return result ;
+  if(i==0 ){
+   return res = U0;
+ }
+  else{
+       let tensionAmont = TensionNoeudAmont(i-1);
+       let chuteTensionNoeudActuel = ChuteTensionNoeud(i);
+       let result = tensionAmont - chuteTensionNoeudActuel;
+  return result ;
+  }
+
+// //Chute de tension relative
+ function ChuteTensionRelative(i){
+
+     return 100*CumulChuteTensionNoeudActuel(i)/U0; 
  }
 
-//Chute de tension relative
-function ChuteTensionRelative(i){
-
-    return 100*CumulChuteTensionNoeudActuel(i)/U0; 
-}
-
-//Intensité Cable
-function IntensiteCable(i,CumulPuiss){
-    //CumulPuiss = Cumul de la puissance au noeud i à calculer à l'aide dela fonction CumulPuissanceNoeud
-    let Result = CumulPuiss/(TensionNoeudActuel(i)*CosPhi);
+// //Intensité Cable
+ function IntensiteCable(i,CumulPuiss){
+     //CumulPuiss = Cumul de la puissance au noeud i à calculer à l'aide dela fonction CumulPuissanceNoeud
+     let Result = CumulPuiss/(TensionNoeudActuel(i)*CosPhi);
     
-    if(NatReseau === "Triphasé"){
-        //Intensité Cable - Triphasé
-        return Result/Math.sqrt(3);
+     if(NatReseau === "Triphasé"){
+         //Intensité Cable - Triphasé
+         return Result/Math.sqrt(3);
+     }
+     else if (NatReseau === "Monophase"){
+         //Intensité Cable - Monophasé
+         return Result;
+     }
+ }
+ }
+
+// TAUX DE CHARGE 
+
+function TauxDeCharge(i , CumulPuiss){
+    let ICable = IntensiteCable(i , CumulPuiss);
+
+    for(let i =0 ; i<lengthTable ; i++){
+        if(section===table[i].section && natureTroncon===table[i].natureTroncon && ameTroncon===table[i].ameTroncon){
+            if(NatReseau==='triphase' && table[i].intensiteNominaleTri !=0 ){
+                let result = 100*ICable/table[i].intensiteNominaleTri;
+                return result;
+            }else if(NatReseau==='monophase' && table[i].intensiteNominaleMono !=0){
+                let result = 100*ICable/table[i].intensiteNominaleMono;
+                return result;
+            }
+            else if(NatReseau==='triphase' && table[i].intensiteNominaleTri ==0){
+                let Inominal = 0      // Creer tableau pour effectuer le calcul
+
+            }
+            else{
+                let Inominal = 0     // Creer tableau pour effectuer le calcul
+            }
+                
+            }
+        }
     }
-    else if (NatReseau === "Monophase"){
-        //Intensité Cable - Monophasé
-        return Result;
-    }
+
+
 }
-}
-
-
-

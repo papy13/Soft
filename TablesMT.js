b6374590afdbbe8a71fb8e871443b12be4ce99cc
papy13 <32386873+papy13@users.noreply.github.com>
2020-04-11 17:45:03 +0200

    Create TablesMT.js

---
+++ TablesMT.js
@@ -0,0 +1,359 @@
+let tableMT =[
+    {
+     section:7.1,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 70
+    },
+    {
+     section:12.6,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable:100
+    },
+    {
+     section:19.6,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable:130
+    },
+    {
+     section:25,
+     natureTroncon:"aerien isolé",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 132
+    },
+
+    {
+     section:25,
+     natureTroncon:"souterrain",
+     ameTroncon:"cuivre",
+     intensiteNominaCable:151
+    },
+    {
+     section:29.3,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 170
+    },
+    {
+     section:34.4,
+     natureTroncon:"aerien nu",
+     ameTroncon:"Almelec",
+     intensiteNominaCable: 145
+    },
+      {
+     section:35,
+     natureTroncon:"aerien isole",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 150
+    },
+    {
+     section:35,
+     natureTroncon:"souterrain",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 145
+    },
+    {
+     section:35,
+     natureTroncon:"aerien isolé",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 195
+    },
+     {
+     section:35,
+     natureTroncon:"souterrain",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 190
+    },
+     {
+     section:37.5,
+     natureTroncon:"aerien nu",
+     ameTroncon:"Alu-acier",
+     intensiteNominaCable: 135
+    },
+     {
+     section:38.2,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 200
+    },
+     {
+     section:48.3,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 230
+    },
+     {
+     section:50,
+     natureTroncon:"aerien isole",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 149
+    },
+     {
+     section:50,
+     natureTroncon:"souterrain",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 168
+    },
+     {
+     section:50,
+     natureTroncon:"aerien isole",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 190
+    },
+     {
+     section:50,
+     natureTroncon:"souterrain",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 215
+    },
+     {
+     section:54.6,
+     natureTroncon:"aerien nu",
+     ameTroncon:"Almelec",
+     intensiteNominaCable: 190
+    },
+     {
+     section:59.7,
+     natureTroncon:"aerien nu",
+     ameTroncon:"Alu-acier",
+     intensiteNominaCable: 155
+    },
+     {
+     section:70,
+     natureTroncon:"aerien isole",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 220
+    },
+     {
+     section:70,
+     natureTroncon:"souterrain",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 210
+    },
+     {
+     section:70,
+     natureTroncon:"aerien isole",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 280
+    },
+     {
+     section:70,
+     natureTroncon:"souterrain",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 270
+    },
+     {
+     section:79.5,
+     natureTroncon:"aerien nu",
+     ameTroncon:"cuivre",
+     intensiteNominaCable: 300
+    },
+     {
+     section:75.5,
+     natureTroncon:"aerien nu",
+     ameTroncon:"Almelec",
+     intensiteNominaCable: 240
+    },
+     {
+     section:75.5,
+     natureTroncon:"aerien nu",
+     ameTroncon:"Alu-acier",
+     intensiteNominaCable: 175
+    },
+    
+     {
+     section:95,
+     natureTroncon:"aerien isole",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 225
+    },
+     {
+     section:95,
+     natureTroncon:"souterrain isole",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 255
+    },
+    {
+        section:95,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 330
+       },
+       {
+        section:95,
+        natureTroncon:"souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 345
+       },
+
+       {
+        section:116.2,
+        natureTroncon:"aerien nu",
+        ameTroncon:"Alu-acier",
+        intensiteNominaCable: 300
+       },
+       
+       {
+        section:117,
+        natureTroncon:"aerien nu",
+        ameTroncon:"Almelec",
+        intensiteNominaCable: 315
+       },
+       {
+        section:120,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 305
+       },
+       {
+        section:120,
+        natureTroncon:"souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 290
+       },
+       {
+        section:120,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 395
+       },
+       {
+        section:120,
+        natureTroncon:"souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 370
+       },
+       {
+        section:147.1,
+        natureTroncon:"aerien nu",
+        ameTroncon:"Alu-acier",
+        intensiteNominaCable: 345
+       },
+       {
+        section:148,
+        natureTroncon:"aerien nu",
+        ameTroncon:"Almelec",
+        intensiteNominaCable: 365
+       },
+       {
+        section:150,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 300
+       },
+       {
+        section:150,
+        natureTroncon:"souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 330
+       },
+       
+       {
+        section:150,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 450
+       },
+       {
+        section:150,
+        natureTroncon:"souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 415
+       },
+
+        {
+     section:185,
+     natureTroncon:"aerien isole",
+     ameTroncon:"Aluminium",
+     intensiteNominaCable: 395
+    },
+    {
+        section:185,
+        natureTroncon:"souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 360
+       },
+       {
+        section:185,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 510
+       },
+       {
+        section:185,
+        natureTroncon:"souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 465
+       },
+       
+       {
+        section:228,
+        natureTroncon:"aerien nu",
+        ameTroncon:"Almelec",
+        intensiteNominaCable: 480
+       },
+       {
+        section:228,
+        natureTroncon:"aerien nu",
+        ameTroncon:"Alu-acier",
+        intensiteNominaCable: 460
+       },
+       
+       {
+        section:240,
+        natureTroncon:"aerien isole",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 330
+       },
+       {
+        section:240,
+        natureTroncon:"souterrain",
+        ameTroncon:"Aluminium",
+        intensiteNominaCable: 440
+       },
+       {
+        section:240,
+        natureTroncon:"aerien isole",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 600
+       },
+       {
+        section:240,
+        natureTroncon:"souterrain",
+        ameTroncon:"cuivre",
+        intensiteNominaCable: 540
+       },  
+]
+
+**********************************************************************************
+
+let TableIntensiteNominale=[
+    {
+        natureTroncon:"aerien nu",
+        ameTroncon:"almelec",
+        k:16.2
+   },
+   {
+    natureTroncon:"aerien nu",
+    ameTroncon:"cuivre",
+    k:22.2
+},
+{
+    natureTroncon:"Alu-acier",
+    ameTroncon:"cuivre",
+    k:14.3
+},
+{
+    natureTroncon:"souterrain",
+    ameTroncon:"aluminium",
+    k:16.4
+},
+{
+    natureTroncon:"souterrain",
+    ameTroncon:"cuivre",
+    k:21
+},
+
+]
